d9088adcdfc9f28995be7701835ae435280c5e76
Santeri Salo <ssanterisalo@gmail.com>
2026-03-30 20:25:36 +0000

    final fetch_f1.py update (hopefully)

--- fetch_f1.py
+++ fetch_f1.py
@@ -1,45 +1,46 @@
 import requests
+from datetime import datetime
 
 def update_dashboard():
-    print("1. Connecting to F1 API...")
-    url = "http://api.jolpi.ca/ergast/f1/2026/driverStandings.json"
+    # 1. Get Standings
+    standings_url = "http://api.jolpi.ca/ergast/f1/2026/driverStandings.json"
+    standings_data = requests.get(standings_url).json()
+    standings = standings_data['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings']
     
-    try:
-        response = requests.get(url)
-        data = response.json()
-        standings = data['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings']
-        print(f"2. Found {len(standings)} drivers.")
+    html_rows = ""
+    for item in standings:
+        html_rows += f"<tr><td>{item['position']}</td><td>{item['Driver']['givenName']} {item['Driver']['familyName']}</td><td>{item['Constructors'][0]['name']}</td><td>{item['points']}</td></tr>\n"
 
-        html_rows = ""
-        for item in standings:
-            pos = item['position']
-            name = f"{item['Driver']['givenName']} {item['Driver']['familyName']}"
-            team = item['Constructors'][0]['name']
-            pts = item['points']
-            html_rows += f"<tr><td>{pos}</td><td>{name}</td><td>{team}</td><td>{pts}</td></tr>\n"
+    # 2. Get Schedule & Find Next Race
+    schedule_url = "http://api.jolpi.ca/ergast/f1/2026.json"
+    schedule_data = requests.get(schedule_url).json()
+    races = schedule_data['MRData']['RaceTable']['Races']
+    
+    next_race = None
+    today = datetime.now()
+    for race in races:
+        race_date = datetime.strptime(race['date'], '%Y-%m-%d')
+        if race_date > today:
+            next_race = race
+            break
 
-        print("3. Reading template.html...")
-        with open("template.html", "r") as f:
-            template_content = f.read()
+    # 3. Read Template and Replace Everything
+    with open("template.html", "r") as f:
+        content = f.read()
 
-        # This is the critical part!
-        if "" in template_content:
-            print("4. Placeholder found! Replacing data...")
-            # We replace the placeholder AND the "If you see this" row
-            final_html = template_content.replace("", html_rows)
-            # This part removes that 'warning' row we added in the template
-            final_html = final_html.replace('<tr><td colspan="4">If you see this, Python hasn\'t replaced the data yet!</td></tr>', "")
-        else:
-            print("ERROR: Could not find '' in template.html")
-            return
+    content = content.replace("", html_rows)
+    
+    if next_race:
+        content = content.replace("", next_race['raceName'])
+        content = content.replace("", next_race['date'])
+        # Creates a filename like 'miami.jpg' from the city name
+        city_image = next_race['Circuit']['Location']['locality'].lower() + ".jpg"
+        content = content.replace("", city_image)
 
-        print("5. Saving to index.html...")
-        with open("index.html", "w") as f:
-            f.write(final_html)
-        print("DONE! Check your index.html now.")
-
-    except Exception as e:
-        print(f"FAILED: {e}")
+    # 4. Save to index.html
+    with open("index.html", "w") as f:
+        f.write(content)
+    print("Dashboard fully updated with Next Race info!")
 
 if __name__ == "__main__":
     update_dashboard()
